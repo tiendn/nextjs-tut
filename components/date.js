7146c3d69d0728756a634f116694bf6d5c8301bf
Tiendn <daonamtien1@gmail.com>
2020-07-16 17:21:02 +0700

    init learn NextJS tutorial

---
+++ components/date.js
@@ -0,0 +1,9 @@
+import dayjs from 'dayjs';
+
+export default function Date({ dateString }) {
+  return (
+    <time dateTime={dateString}>
+      {dayjs(dateString).format('MMMM d, YYYY')}
+    </time>
+  );
+}
